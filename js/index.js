38b4b466ba53c7df01bcbd5d003d7861a81817f5
Nitya Dholakia <ndholakia97@gmail.com>
2021-06-19 00:04:32 -0700

    animations added
    -scroll to top
    - View Work button
    -preloader animation

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
 console.log("Your index.js file is loaded correctly!");
 var document = window.document;
+
+/*==== Back to Top ====*/
+mybutton = document.getElementById("toTop");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+/*==== Preloader ====*/
+
+function fade() {
+	$('.preloader').fadeOut("slow");
+	}
+	setTimeout(fade, 1000);
